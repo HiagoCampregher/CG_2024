b18961e0e95b45feb8f647c5933d541974fd9261
unknown <campregherhiago@gmail.com>
2024-04-14 20:57:13 -0300

    continuacao v2

---
+++ Exercicio3/SrPalito.cs
@@ -0,0 +1,20 @@
+﻿using CG_Biblioteca;
+using System;
+using OpenTK.Graphics.OpenGL4;
+
+namespace gcgcg
+{
+    internal class SrPalito : Objeto
+    {
+        public SrPalito(Objeto _paiRef, ref char _rotulo) : base(_paiRef, ref _rotulo)
+        {
+            Atualizar();
+        }
+
+        private void Atualizar()
+        {
+
+            base.ObjetoAtualizar();
+        }
+    }
+}
